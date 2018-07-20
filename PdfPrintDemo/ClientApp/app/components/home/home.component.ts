fd88e17739e8a5b6066030890f25808f17465adf
Madan Thapa <thapamadan@gmail.com>
2018-07-20 07:53:07 -0500

    Added pdfMake

--- PdfPrintDemo/ClientApp/app/components/home/home.component.ts
+++ PdfPrintDemo/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+    printPdf(): void {
+        var docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+        pdfMake.createPdf(docDefinition).download();
+    }
 }
---
+++ PdfPrintDemo/ClientApp/app/typings.ts
@@ -0,0 +1,2 @@
+﻿declare module 'pdfmake/build/pdfmake.js';
+declare module 'pdfmake/build/vfs_fonts.js';
